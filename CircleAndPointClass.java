685b87787d13bb1a7596fea74193f7506659ee86
Abhinab Baruah <abhinabbaruah920@gmail.com>
2026-04-03 14:53:02 +0530

    Implement Circle and Point classes with display method

---
+++ CircleAndPointClass.java
@@ -0,0 +1,32 @@
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Circle {
+    Point center;
+    double radius;
+
+    Circle(int x, int y, double r) {
+        center = new Point(x, y);
+        radius = r;
+    }
+
+    void display() {
+        double area = Math.PI * radius * radius;
+        System.out.println("Center: (" + center.x + ", " + center.y + ")");
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + area);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle(2, 3, 5);
+        c.display();
+    }
+}
